4095118632071ad4167561a0b9adec73d964987c
Vera Kratz <vera.kratz12@gmail.com>
2018-12-05 13:47:23 +0100

    Add icon

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStroopwafel)
+
+const Anchor = styled.div`
+  color: grey;
+`
 
 export default class Bookmark extends Component {
   render() {
-    return <div onClick={this.props.handleOnClick}>{'*'}</div>
+    return (
+      <Anchor onClick={this.props.handleOnClick}>
+        <FontAwesomeIcon icon="" />
+      </Anchor>
+    )
   }
 }
